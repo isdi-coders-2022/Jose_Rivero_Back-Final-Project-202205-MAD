a30f940bf36d933bb86efdd3b070e76a69290c12
JoseRivero <josearmandorr95@gmail.com>
2022-07-04 17:28:19 +0200

    add auth y bcrypts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AuthService } from 'src/auth/auth.service';
+import { BcryptService } from 'src/auth/bcrypt.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { iUser } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private readonly User: Model<iUser>) {}
+    constructor(
+        @InjectModel('User') private readonly User: Model<iUser>,
+        private readonly auth: AuthService,
+        private readonly bcrypt: BcryptService
+    ) {}
+
     async create(createUserDto: CreateUserDto) {
-        return this.User.create(createUserDto);
+        const newUser = await this.User.create({
+            ...createUserDto,
+            password: this.bcrypt.encrypt(createUserDto.password),
+        });
+        const token = this.auth.createToken(newUser.id);
+        return {
+            user: newUser,
+            token,
+        };
     }
 
     findAll() {
